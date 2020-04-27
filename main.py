2dbb3c906e9aed3938be647ce0495a13da0ef1f3
Erik Vroon <erik.vroon22@gmail.com>
2020-04-27 19:33:19 +0200

    Improve documentation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 import cv2
 import imageio
-from matplotlib import pyplot as plt
-from PIL import Image, ImageDraw
 import warnings
 import timeit
 import os
@@ -14,7 +14,15 @@ class detection:
 
 
     def apply_filter(self, img):
-        ''' Create the filter mask to filter the sides of the gates and apply it to the image. '''
+        ''' Filter the sides of the gates and apply it to the image.
+
+        Args:
+            img(Image): the image to filter
+
+        Returns:
+            img(Image): the image where the side of the gates are filtered
+        '''
+
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         lower_blue = np.array([96, 145, 145])
@@ -29,11 +37,19 @@ class detection:
         '''
         Detect the gates in the image given by image_index.
 
-        Returns the original image with gates segmented in red.
+        Args:
+            image_index(int): which image to load
+            threshold(float): the maximum penalty for gates to be accepted
+
+        Returns:
+            img: the original image with gates segmented in red
+            TPR: the true positive ratio
+            FPR: the false positive ratio
         '''
 
         img = cv2.imread('WashingtonOBRace/img_{}.png'.format(image_index))
 
+        # Stop if image could not be read.
         if not isinstance(img, np.ndarray):
             return False, np.nan, np.nan
 
@@ -43,10 +59,12 @@ class detection:
 
         filtered_img = self.apply_filter(img)
 
+        # Calculate the contours.
         _, threshold_img = cv2.threshold(filtered_img, 50, 100, 0)
         contours, _ = cv2.findContours(threshold_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         pillars = []
 
+        # Process all contours.
         for contour in contours:
             width = np.max(contour[:, 0, 0]) - np.min(contour[:, 0, 0])
             height = np.max(contour[:, 0, 1]) - np.min(contour[:, 0, 1])
@@ -54,6 +72,7 @@ class detection:
             if height < 1.7 * width:
                 continue
 
+            # Calculate the extrema of the contour.
             heighest = np.argmax(contour[:, 0, 1])
             lowest = np.argmin(contour[:, 0, 1])
 
@@ -63,6 +82,7 @@ class detection:
             center_outer = 0.5 * (top_outer + bottom_outer)
             center_x = 0.5 * (np.min(contour[:, 0, 0]) + np.max(contour[:, 0, 0]))
 
+            # Extrapolate the calculated vertices to get the vertices of the entire pillar.
             parallel = top_outer - bottom_outer
             bottom_extrapolated = center_outer - parallel
             top_extrapolated = center_outer + parallel
@@ -78,6 +98,7 @@ class detection:
         output_img = np.zeros((img.shape[0], img.shape[1]))
         used_right_pillars = []
 
+        # Process all detected pillars.
         for i in range(len(pillars)):
             leftpillar = pillars[i]
             penalties = []
@@ -89,15 +110,17 @@ class detection:
             height = topleft[1] - bottomleft[1]
 
             if leftpillar[0]:
-                for j in range(0, len(pillars)):
+                for j in range(len(pillars)):
                     rightpillar = pillars[j]
 
-                    # pillar must not be already used, be a right pillar,
+                    # Right pillar must not be already used, be a right pillar and
+                    # have approximately the same dimensions as the left pillar.
                     if j not in used_right_pillars and \
                         not rightpillar[0] and \
                         rightpillar[3] < 1.25 * leftpillar[3] and \
                         rightpillar[3] > 0.75 * leftpillar[3]:
 
+                        # Calculate the vertices of the gate in image coordinates.
                         topright = np.array(rightpillar[1])
                         bottomright = np.array(rightpillar[2])
 
@@ -119,6 +142,7 @@ class detection:
                         edge_length[2] = np.sqrt(np.sum((bottomleft - bottomright) ** 2))
                         edge_length[3] = np.sqrt(np.sum((bottomright - topright) ** 2))
 
+                        # Calculate the penalty that is to be minimized.
                         penalty = np.std(edge_length) / np.average(edge_length)
 
                         used_right_pillars.append(j)
@@ -136,17 +160,20 @@ class detection:
                 polygon_inner = inner_polygons[best_match]
 
                 if self.save_output:
+                    # Render the polygon of the detected gate onto a new image.
                     draw_img = Image.new('L', img.shape[1::-1], 0)
                     draw = ImageDraw.Draw(draw_img)
                     draw.polygon(polygon_outer, outline=1, fill=1)
                     draw.polygon(polygon_inner, outline=1, fill=0)
                     draw_img = np.array(draw_img)
 
+                    # Save output and overlay polygon in original image in red.
                     output_img = output_img + draw_img
                     img[np.array(draw_img) == 1, :] = [0, 0, 255]
 
 
         if self.save_output:
+            # Calculate the TPR and FPR values.
             output_img[output_img > 1] = 1
             true_positives = np.sum(output_img * mask)
             false_positives = np.sum(output_img * (255 - mask))
@@ -154,10 +181,20 @@ class detection:
 
         return img, np.nan, np.nan
 
-    def process(self, img_list=None, thresholds=[0.15]):
-        ''' Process a list of images and output the resulting segmented images '''
 
-        for threshold in thresholds:
+    def process(self, img_list=None, thresholds=[0.15]):
+        ''' Process a list of images and output the resulting segmented images.
+
+        Args:
+            img_list (list): The images to process. If it is None, all images will be processed
+            thresholds (list): The thresholds for which to calculate the ROC curve
+
+        The TPR and FPR data are stored in the 'results' directory.
+        '''
+
+        self.thresholds = thresholds
+
+        for threshold in self.thresholds:
             plot_data = []
 
             if img_list == None:
@@ -173,25 +210,23 @@ class detection:
                     cv2.imwrite('results/result_{}.png'.format(str(image)), img)
 
             stop = timeit.default_timer()
-            # print('Computation time: ', stop - start, 's')
             print('Computation time per image: ', (stop - start) / 308 * 1e3, 'ms')
 
             np.save('results/data_{:.2f}'.format(threshold), plot_data)
 
 
     def plot(self):
-        ''' Plot the ROC curve '''
+        ''' Plots the ROC curve for different thresholds. '''
 
         if not self.save_output:
             return
 
-        for threshold in thresholds:
+        for threshold in self.thresholds:
             # Ignore warnings about nan values.
             warnings.filterwarnings('ignore')
 
-            data_file = 'results/data_{:.2f}.npy'.format(threshold)
-
             # Load data
+            data_file = 'results/data_{:.2f}.npy'.format(threshold)
             plot_data = np.load(data_file)
 
             x = np.array([item[0] for item in plot_data])
@@ -213,8 +248,10 @@ class detection:
             no_gates_count = np.sum(x == 0)
             label = 'threshold: {:2.2f}, {:3} images without detections'.format(threshold, no_gates_count)
 
+            # Remove nan values.
             avg_std = avg_std[avg_std[:, 0] <= 1.0, :]
 
+            # Plot errorbars only for the optimal threshold.
             if threshold == 0.15:
                 plt.errorbar(avg_std[:, 0], avg_std[:, 1], avg_std[:, 2], avg_std[:, 3],
                     marker='o', markersize=6, capsize=3, barsabove=False, label=label, zorder=1, color='indigo')
@@ -229,7 +266,10 @@ class detection:
         plt.legend()
         plt.savefig('roc', bbox_inches='tight')
 
-thresholds = [0.06, 0.1, 0.15, 0.25]
+
+# Put the optimal threshold (0.15) at the end such that
+# the optimal images are not overwritten by other results.
+thresholds = [0.06, 0.1, 0.25, 0.15]
 
 det = detection()
 det.process(None, thresholds)
